b3da25b0518ce91c7773ba4a4d02f0c89121fd3c
bryanat <boyettbryan@gmail.com>
2023-01-03 00:33:22 -0500

    Remove practice/ directory

--- Assets/ML-Agents/practice/Scripts/PickUpScript.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PickupScript : MonoBehaviour
-{
-   public bool isHeld = false; //whether the object is held by the agent or not
-   public bool isOrganized  = false; //whether the box is stacked or not
-}
