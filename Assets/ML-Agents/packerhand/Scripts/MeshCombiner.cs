c50b741c83a631deea9b7e9eb46db78d18b0f2e7
Pano <PanoEvJ>
2023-01-08 00:54:33 +0100

    combineMeshes()

---
+++ Assets/ML-Agents/packerhand/Scripts/MeshCombiner.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshCombiner : MonoBehaviour
+{
+    // The list of meshes to combine
+    public List<GameObject> meshList;
+
+    void Start()
+    {
+
+        Debug.Log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%   START MESH COMBINING   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+        Debug.Log($" GameObject name ==== {gameObject.name}");
+
+        // Create a list of CombineInstance structures
+        List<CombineInstance> combine = new List<CombineInstance>();
+
+        // Loop through all the meshes in the list
+        for (int i = 0; i < meshList.Count; i++)
+        {
+
+            Debug.Log($" Listed object name ==== {meshList[i].name}");
+
+            // Get the mesh and its transform component
+            Mesh mesh = meshList[i].GetComponent<MeshFilter>().mesh;
+            Transform transform = meshList[i].transform;
+
+            // Create a new CombineInstance and set its properties
+            CombineInstance ci = new CombineInstance();
+            ci.mesh = mesh;
+            ci.transform = transform.localToWorldMatrix;
+
+            // Add the CombineInstance to the list
+            combine.Add(ci);
+        }
+
+        // Create a new mesh on the GameObject
+        MeshFilter mf = this.GetComponent<MeshFilter>();
+        MeshRenderer mr = this.GetComponent<MeshRenderer>();
+
+        // // Set the materials of the new mesh to the materials of the original meshes
+        // Material[] materials = new Material[meshList.Count];
+        // for (int i = 0; i < meshList.Count; i++)
+        // {
+        //     materials[i] = meshList[i].GetComponent<Renderer>().sharedMaterial;
+        // }
+        // mr.materials = materials;
+
+        // mf       --> original object
+        // meshList --> StopAllCoroutines the objects to be combined into the mf
+
+        // Combine the meshes
+        mf.mesh.CombineMeshes(combine.ToArray(), true, true);
+        Debug.Log($"mf position === {mf.transform.position}");
+
+    Debug.Log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%   MESH IS COMBINED   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+
+    }
+}
