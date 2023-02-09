2fe22a5806ac67243bc88eb6bbd565b31dadd92e
Pano <PanoEvJ>
2023-02-09 12:57:13 +0100

    test 412

--- Assets/ML-Agents/packerhand/Scripts/PackerHand.cs
+++ Assets/ML-Agents/packerhand/Scripts/PackerHand.cs
@@ -97,6 +97,7 @@ public class PackerHand : Agent
     public float binscale_y;
     public float binscale_z;
     public Vector3 origin;
+    List<int> vertexIndices;
 
 
     public override void Initialize()
@@ -166,7 +167,6 @@ public class PackerHand : Agent
         
     }
 
-
     /// <summary>
     /// Agent adds environment observations 
     /// </summary>
@@ -184,12 +184,20 @@ public class PackerHand : Agent
         }
 
         // // array of vertices
+        int i = 0;
+        vertexIndices = new List<int>();
         foreach (Vector3 vertex in verticesArray) {
             Vector3 scaled_continous_vertex = new Vector3(((vertex.x - origin.x)/binscale_x), ((vertex.y - origin.y)/binscale_y), ((vertex.z - origin.z)/binscale_z));
+    
             //Debug.Log($"XYZ scaled_continous_vertex: {scaled_continous_vertex}");
             // verticesArray is still getting fed vertex: (0, 0, 0) which is scaled_continous_vertex: (-0.35, -0.02, -0.18)
 
             sensor.AddObservation(vertex); //add vertices to sensor observations
+            if ( scaled_continous_vertex == new Vector3(-0.35f, -0.02f, -0.18f))
+            {
+                vertexIndices.Add(i);
+            }
+            i++;
         }
 
         // // array of blackboxes 
@@ -204,6 +212,13 @@ public class PackerHand : Agent
         //     sensor.AddObservation(blackbox.vertex);
         // }
         // sensor.AddObservation(blackboxesArray); //add vertices to sensor observations
+    }
+
+    public override void WriteDiscreteActionMask(IDiscreteActionMask actionMask)
+    {
+        foreach (int vertexIdx in vertexIndices) {
+            actionMask.SetActionEnabled(0, vertexIdx, false);
+        }
     }
 
     public override void OnActionReceived(ActionBuffers actionBuffers)
@@ -247,6 +262,11 @@ public class PackerHand : Agent
             {
                 Debug.Log("TEBS FINISHED PACKING ALL BOXES IN ONE EPISODE ");
             }
+            else 
+            {
+                Debug.Log("TEBS MAX NO. OF STEPS EXCEEDED ");
+            }
+            AddReward(-30f);
             EndEpisode();
         }
         // Initialize curriculum and brain
@@ -320,7 +340,9 @@ public class PackerHand : Agent
                 }
                 else
                 {
-                    BoxReset("failedPhysicsCheck");
+                    BoxReset("failedPhysicsCheck &&&&&&&&&& restart episode");
+                    AddReward(-30f);
+                    EndEpisode();
                 }
             }
         }
