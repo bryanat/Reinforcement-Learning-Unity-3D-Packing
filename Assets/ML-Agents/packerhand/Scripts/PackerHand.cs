d713d544fa343b462761edd1392d6fd6c6328273
Pano <PanoEvJ>
2023-02-10 02:23:03 +0100

    tuning rewards

--- Assets/ML-Agents/packerhand/Scripts/PackerHand.cs
+++ Assets/ML-Agents/packerhand/Scripts/PackerHand.cs
@@ -335,8 +335,8 @@ public class PackerHand : Agent
                     // add surface area reward
                     box_surface_area = 2*boxWorldScale.x*boxWorldScale.y + 2*boxWorldScale.y * boxWorldScale.z + 2*boxWorldScale.x *  boxWorldScale.z;
                     percent_contact_surface_area = sensorCollision.totalContactSA/box_surface_area;
-                    AddReward(percent_contact_surface_area*10f);
-                    Debug.Log($"RWDSA {GetCumulativeReward()} total reward | {sensorCollision.totalContactSA/box_surface_area*10f} reward from surface area");
+                    AddReward(percent_contact_surface_area*20f);
+                    Debug.Log($"RWDSA {GetCumulativeReward()} total reward | {sensorCollision.totalContactSA/box_surface_area*20f} reward from surface area");
                     DropoffBox();
                 }
                 else
