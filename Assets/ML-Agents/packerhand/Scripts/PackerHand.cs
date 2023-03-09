cf3f73bc3e2ad843160c0eeae0919e4ae53fa632
Pano <PanoEvJ>
2023-03-09 04:27:04 +0100

    temp example

--- Assets/ML-Agents/packerhand/Scripts/PackerHand.cs
+++ Assets/ML-Agents/packerhand/Scripts/PackerHand.cs
@@ -167,6 +167,7 @@ public class PackerHand : Agent
     private int observable_size=8; // The observable size of an entity used in the attention mechanism
     private int max_observable_size; // Default                  : observable_size
                                      // OneHotEncoding || Padding: observable_size + maxBoxQuantity
+    // private int temp=55; // delete this variable and the print statement associated with it
 
 
 
@@ -250,6 +251,10 @@ public class PackerHand : Agent
         num_boxes_y = boxSpawner.num_boxes.y;
         num_boxes_z = boxSpawner.num_boxes.z;
 
+        isEpisodeStart = true;
+
+        Debug.Log("INITIALIZE ENDS");
+
         if (useAttention){
             m_BufferSensor = GetComponent<BufferSensorComponent>();
             // Set (in the Inspector) the maximum number of observables entities that the BufferSensor can observe
@@ -264,17 +269,20 @@ public class PackerHand : Agent
                 max_observable_size = observable_size;}
             m_BufferSensor.ObservableSize = max_observable_size;
         }
-
-        isEpisodeStart = true;
-
-        Debug.Log("INITIALIZE ENDS");
     }
-
 
     public override void OnEpisodeBegin()
     {   
         Debug.Log("-----------------------NEW EPISODE STARTS------------------------------");
-      
+
+        // // Temporary testing: checking if the buffer size can be updated during runtime
+        // //  (increase buffer size during runtime but keep number of variable observations the same)
+        // if (useAttention){
+        //     temp = temp + 11;
+        //     m_BufferSensor.ObservableSize = temp;
+        //     Debug.Log($"Observable size: {m_BufferSensor.ObservableSize}");
+        //     // .....remove above block
+        // }
     }
 
 
@@ -301,6 +309,7 @@ public class PackerHand : Agent
             if (useAttention){
                 int idx_cntr = 0;
                 float[] listVarObservation = new float[max_observable_size];
+
                 if (useOneHotEncoding){
                     // Used for variable size observations
                     int boxNum = int.Parse(box.rb.name);
@@ -338,6 +347,7 @@ public class PackerHand : Agent
                 sensor.AddObservation( (box.boxSize.x/binscale_x)*(box.boxSize.y/binscale_y)*(box.boxSize.z/binscale_z) );
                 sensor.AddObservation (box.boxVertex);
             }
+
             // Add boxes to action ask: 1) boxes already placed in the bin, 2) boxes that are zero-padded
             if (box.isOrganized){
                 maskedBoxIndices.Add(j);}
@@ -369,19 +379,6 @@ public class PackerHand : Agent
                 i++;
             }
         }
-
-        // // array of blackboxes 
-        // foreach (Blackbox blackbox in blackboxPool)
-        // {
-        //     // float[][] blackbox_observation = new float[][]{};
-        //     // blackbox_observation = new float[][] {
-        //     //     new float[] {blackbox.size.x, blackbox.size.y, blackbox.size.z},
-        //     //     new float[] {blackbox.vertex.x, blackbox.vertex.y, blackbox.vertex.z},
-        //     // };
-        //     sensor.AddObservation(blackbox.size);
-        //     sensor.AddObservation(blackbox.vertex);
-        // }
-        // sensor.AddObservation(blackboxesArray); //add vertices to sensor observations
     }
 
 
@@ -736,76 +733,6 @@ public class PackerHand : Agent
             }
         }
     }
-
-
-    // public void UpdateBlackBox() 
-    // {
-    //     Debug.Log($"UBX Update BlackboX running");
-
-        // foreach (Vector3 vertex in verticesArray) 
-        // {
-        //     //bottomVertices.Find(v=>  v[1]==vertex[1] && v[2]==vertex[2]).MinBy(v => Math.Abs(v[0]-vertex[0]));
-        //     Vector3 closest_x_vertex = backMeshVertices.Aggregate(new Vector3(float.MaxValue, 0, 0), (min, next) => 
-        //     vertex[0]<next[0] && Math.Abs(next[0]-vertex[0]) < Math.Abs(min[0] - vertex[0]) && next[1]==vertex[1] && next[2] == vertex[2] ? next : min);
-        //     //Debug.Log($"BCX BLACK BOX VERTEX IS {vertex} AND CLOSES X VERTEX IS {closest_x_vertex}");
-
-        //     Vector3 closest_y_vertex = sideMeshVertices.Aggregate(new Vector3(0, float.MaxValue, 0), (min, next) => 
-        //     vertex[1]<next[1] && Math.Abs(next[1]-vertex[1]) < Math.Abs(min[1] - vertex[1]) && next[0]==vertex[0] && next[2] == vertex[2] ? next : min);
-        //     //Debug.Log($"BCX BLACK BOX VERTEX IS {vertex} AND CLOSES Y VERTEX IS {closest_y_vertex}");
-
-        //     Vector3 closest_z_vertex = sideMeshVertices.Aggregate(new Vector3(0, 0, float.MaxValue), (min, next) => 
-        //     vertex[2]<next[2] && Math.Abs(next[2]-vertex[2]) < Math.Abs(min[2] - vertex[2]) && next[1]==vertex[1] && next[0] == vertex[0] ? next : min);
-        //     //Debug.Log($"BCX BLACK BOX VERTEX IS {vertex} AND CLOSES Z VERTEX IS {closest_z_vertex}");
-
-        //     float blackbox_x_size = Math.Abs(closest_x_vertex[0] - vertex[0]);
-        //     float blackbox_y_size = Math.Abs(closest_y_vertex[1] - vertex[1]);
-        //     float blackbox_z_size = Math.Abs(closest_z_vertex[2] - vertex[2]);
-        //     Vector3 blackbox_position = new Vector3(blackbox_x_size*0.5f+vertex[0], blackbox_y_size*0.5f+vertex[1], blackbox_z_size*0.5f+vertex[2]);
-
-        //     if (blackbox_x_size<100f && blackbox_x_size>2f && blackbox_y_size<100f && blackbox_y_size > 2f && blackbox_z_size<100f && blackbox_z_size>2f) 
-        //     {
-        //         Debug.Log($"BPS BLACK BOX POSITION {blackbox_position} SIZES {blackbox_x_size}, {blackbox_y_size}, {blackbox_z_size}");
-        //         GameObject blackbox = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        //         blackbox.name = "blackbox";
-        //         blackbox.transform.position = blackbox_position;
-        //         blackbox.transform.localScale = new Vector3(blackbox_x_size, blackbox_y_size, blackbox_z_size);
-        //         Renderer cubeRenderer = blackbox.GetComponent<Renderer>();
-        //         cubeRenderer.material = clearPlastic;
-
-        //         Blackbox newBlackbox = new Blackbox
-        //         {
-        //             position = blackbox_position,
-        //             size = new Vector3(blackbox_x_size, blackbox_y_size, blackbox_z_size),
-        //             vertex = vertex,
-        //             gameobjectBlackbox = blackbox,
-        //             volume = blackbox_x_size * blackbox_y_size * blackbox_z_size,
-        //         };
-
-        //         blackboxPool.Add(newBlackbox);
-        //     }
-        // }
-        // isBlackboxUpdated = true;
-    // }
-
-    // public void SelectBlackboxVertex() 
-    // {
-        
-    //     Blackbox smallest_blackbox = null;
-    //     float minVolume = float.MaxValue;
-    //     foreach (Blackbox blackbox in blackboxPool)
-    //     {
-    //         if (blackbox.volume < minVolume)
-    //         {
-    //             minVolume = blackbox.volume;
-    //             smallest_blackbox = blackbox;
-    //         }
-    //     }
-    //     Debug.Log($"SBV SMALLEST BLACKBOX IS: {smallest_blackbox.gameobjectBlackbox} with volume {smallest_blackbox.volume} and vertex {smallest_blackbox.vertex}");
-    //     smallest_blackbox.gameobjectBlackbox.GetComponent<Renderer>().material.color = Color.black;
-    //     selectedVertex = smallest_blackbox.vertex;
-    //     isVertexSelected = true;
-
-    // }
 
     public void SelectVertexDiscrete(int action_SelectedVertexIdx, float action_SelectedVertex_x, float action_SelectedVertex_y, float action_SelectedVertex_z) 
     {
