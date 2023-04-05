08b1db0bea1217ab5487f8d883d419dc5763248c
happinessbaby <yueqipeng2021@gmail.com>
2023-04-05 13:47:38 -0500

    cleanup

--- Assets/ML-Agents/packerhand/Scripts/PackerHand.cs
+++ Assets/ML-Agents/packerhand/Scripts/PackerHand.cs
@@ -89,8 +89,6 @@ public class PackerHand : Agent
     public float percent_filled_bin_volume;
     public float percent_contact_surface_area;
     public int boxes_packed;
-    // int episode_to_export = 0; 
-    //bool ready_for_export = false;
     string homeDir;
 
 
@@ -347,7 +345,6 @@ public class PackerHand : Agent
         {
             if (AppHelper.StartTimer("training"))
             {
-                //ExportResult();
                 EndTraining();
                 // switch to inference for production: run commands on same run-id with inference flag
             }
@@ -356,15 +353,9 @@ public class PackerHand : Agent
         {
             if (percent_filled_bin_volume > AppHelper.threshold_volume)
             {
-                //ready_for_export = true; 
                 EndTraining();
                 // switch to inference for production
             } 
-            //if (ready_for_export)
-            // {
-            //     //ExportResult();
-            //     EndTraining();
-            // }
         }
         else if (isInference)
         {
@@ -1124,13 +1115,6 @@ public class PackerHand : Agent
 
     public void ExportResult()
     {
-        // if (isInference)
-        // {
-        //     // when running inference, first episode's result will be exported
-        //     episode_to_export = 2;
-        // }
-        // if (CompletedEpisodes == episode_to_export)
-        // {
         if (CompletedEpisodes == 2)
         {
             // if file has not been exported, export fbx
@@ -1148,11 +1132,6 @@ public class PackerHand : Agent
                 AppHelper.LogStatus("instructions");
             }
         }
-        // for training, get which end of episode to export
-        // if (episode_to_export == 0)
-        // {
-        //     episode_to_export = CompletedEpisodes+2;
-        // }
     }
 
     public void EndTraining()
